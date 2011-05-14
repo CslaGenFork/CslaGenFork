2ed0e83d59f15a2131858da239e09e84e74bb744
tiago_cp <SND\tiago_cp>
2011-05-14 09:54:26 +0000

    Added public helper methods to get all possible parents or children of a given object or object type.

--- trunk/Solutions/CslaGenFork/Metadata/CslaObjectInfoCollection.cs
+++ trunk/Solutions/CslaGenFork/Metadata/CslaObjectInfoCollection.cs
@@ -18,9 +18,55 @@ namespace CslaGenerator.Metadata
 
         public List<string> GetAllObjectNames()
         {
-            List<String> lst = new List<string>();
+            var lst = new List<string>();
             foreach (CslaObjectInfo obj in this)
                 lst.Add(obj.ObjectName);
+            return lst;
+        }
+
+        public List<string> GetAllParentNames(CslaObjectInfo info)
+        {
+            var lst = new List<string>();
+            foreach (CslaObjectInfo obj in this)
+            {
+                if (RelationRulesEngine.IsParentAllowed(obj.ObjectType, info.ObjectType) &&
+                    obj != info)
+                    lst.Add(obj.ObjectName);
+            }
+            return lst;
+        }
+
+        public List<string> GetAllParentNames(CslaObjectType cslaType)
+        {
+            var lst = new List<string>();
+            foreach (CslaObjectInfo obj in this)
+            {
+                if (RelationRulesEngine.IsParentAllowed(obj.ObjectType, cslaType))
+                    lst.Add(obj.ObjectName);
+            }
+            return lst;
+        }
+
+        public List<string> GetAllChildNames(CslaObjectInfo info)
+        {
+            var lst = new List<string>();
+            foreach (CslaObjectInfo obj in this)
+            {
+                if (RelationRulesEngine.IsChildAllowed(obj.ObjectType, info.ObjectType) &&
+                    obj != info)
+                    lst.Add(obj.ObjectName);
+            }
+            return lst;
+        }
+
+        public List<string> GetAllChildNames(CslaObjectType cslaType)
+        {
+            var lst = new List<string>();
+            foreach (CslaObjectInfo obj in this)
+            {
+                if (RelationRulesEngine.IsChildAllowed(obj.ObjectType, cslaType))
+                    lst.Add(obj.ObjectName);
+            }
             return lst;
         }
 
