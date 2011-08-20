d92700be9c46db77c56ff449d4f59b66c853b98a
tiago_cp <SND\tiago_cp>
2011-08-20 17:08:09 +0000

    Preparing for Nightly Build 2011.08.20

--- trunk/Solutions/CslaGenFork/AboutBox.cs
+++ trunk/Solutions/CslaGenFork/AboutBox.cs
@@ -17,7 +17,7 @@ namespace CslaGenerator
             lblAppDescription.Text = GetAttr<AssemblyDescriptionAttribute>().Description;
             lblAppCopyright.Text = GetAttr<AssemblyCopyrightAttribute>().Copyright;
             lblAssyVersion.Text = Assembly.GetExecutingAssembly().FullName.Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)[1].Replace("=", " ");
-            lblAssyVersion.Text += @" (Nightly Build 2011.08.01)";
+            lblAssyVersion.Text += @" (Nightly Build 2011.08.20)";
         }
         
         private T GetAttr<T>() {            
