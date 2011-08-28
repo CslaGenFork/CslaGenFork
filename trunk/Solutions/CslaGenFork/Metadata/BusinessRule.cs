da6b34c2871db0feb8dd3798672ab58c69e2c2dd
Tiago Freitas Leal <tfreitasleal@gmail.com>
2011-08-28 08:09:54 +0000

    Idem.

--- trunk/Solutions/CslaGenFork/Metadata/BusinessRule.cs
+++ trunk/Solutions/CslaGenFork/Metadata/BusinessRule.cs
@@ -87,7 +87,7 @@ namespace CslaGenerator.Metadata
         [Category("01. Definition")]
         [Description("Business Rule Type defined in this CslaGenFork project. Unsupported at this time.")]
         [UserFriendlyName("Internal project Type Name")]
-        [Browsable(true)]
+        [Browsable(false)]
         public string ObjectName
         {
             get { return _objectName; }
