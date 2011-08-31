ea9413468cbd19ea98cb207ce5e99cbe2eaf2d33
Tiago Freitas Leal <tfreitasleal@gmail.com>
2011-08-31 21:53:23 +0000

    Rules sample: tag v.1.0.0.1

---
+++ tags/Rules sample v.1.0.0.1/CslaGenFork.Rules-4.1.0/Authorization Rules/IsOwnerOrIsInRole.cs
@@ -0,0 +1,108 @@
+﻿/*
+License: The MIT License (MIT) 
+Copyright (c) 2011 CslaGenFork project.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  
+*/
+
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IsOwnerOrIsInRole.cs" company="">
+//   Copyright (c) 2011 CslaGenFork project. Website: http://cslagenfork.codeplex.com/
+// </copyright>
+// <summary>
+//   Authorization rule for checking a Property can be accessed only
+//   by the user that created the object or the user has any of the specified roles.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Csla;
+using Csla.Core;
+using Csla.Reflection;
+using Csla.Rules;
+
+namespace CslaGenFork.Rules.AuthorizationRules
+{
+    /// <summary>
+    /// Authorization rule for checking a Property can be accessed only
+    /// by the user that created the object or the user has any of the specified roles.
+    /// </summary>
+    public class IsOwnerOrIsInRole : AuthorizationRule
+    {
+        private readonly List<string> _roles;
+        private string _creatorProperty;
+        private Func<int> _getCurrentUserDelegate;
+
+        /// <summary>
+        /// Creates an instance of the rule.
+        /// </summary>
+        /// <param name="action">Action this rule will enforce.</param>
+        /// <param name="element">Member to be authorized.</param>
+        /// <param name="creatorProperty">Name of the property for the creator ID.</param>
+        /// <param name="getCurrentUserDelegate">The Func delegate to get the current user ID.</param>
+        /// <param name="roles">List of allowed roles.</param>
+        public IsOwnerOrIsInRole(AuthorizationActions action, IMemberInfo element, string creatorProperty,
+                                 Func<int> getCurrentUserDelegate, List<string> roles)
+            : base(action, element)
+        {
+            _creatorProperty = creatorProperty;
+            _getCurrentUserDelegate = getCurrentUserDelegate;
+            _roles = roles;
+        }
+
+        /// <summary>
+        /// Creates an instance of the rule.
+        /// </summary>
+        /// <param name="action">Action this rule will enforce.</param>
+        /// <param name="element">Member to be authorized.</param>
+        /// <param name="creatorProperty">Name of the property for the creator ID.</param>
+        /// <param name="getCurrentUserDelegate">The Func delegate to get the current user ID.</param>
+        /// <param name="roles">List of allowed roles.</param>
+        public IsOwnerOrIsInRole(AuthorizationActions action, IMemberInfo element, string creatorProperty,
+                                 Func<int> getCurrentUserDelegate, params string[] roles)
+            : base(action, element)
+        {
+            _creatorProperty = creatorProperty;
+            _getCurrentUserDelegate = getCurrentUserDelegate;
+            _roles = new List<string>(roles);
+        }
+
+        /// <summary>
+        /// Rule implementation.
+        /// </summary>
+        /// <param name="context">Authorization context.</param>
+        protected override void Execute(AuthorizationContext context)
+        {
+            var creatorID = (int) MethodCaller.CallPropertyGetter(context.Target, _creatorProperty);
+            var currentUserID = _getCurrentUserDelegate.Invoke();
+            if (currentUserID == creatorID)
+                context.HasPermission = true;
+
+            if (_roles.Count > 0)
+            {
+                if (_roles.Any(item => ApplicationContext.User.IsInRole(item)))
+                {
+                    context.HasPermission = true;
+                }
+            }
+            else
+            {
+                // if no role specified, allow all roles
+                context.HasPermission = true;
+            }
+        }
+    }
+}
---
+++ tags/Rules sample v.1.0.0.1/CslaGenFork.Rules-4.2.0/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CslaGenFork.Rules-4.2.0")]
+[assembly: AssemblyDescription("Business and Authorization Rules library for Csla 4.2.0")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CslaGenFork Project")]
+[assembly: AssemblyProduct("CslaGenFork.Rules-4.2.0")]
+[assembly: AssemblyCopyright("Copyright © CslaGenFork Project, 2011")]
+[assembly: AssemblyTrademark("All Rights Reserved.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2db5bebd-cbc5-4cdb-b7e2-3aa986a5a5aa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
