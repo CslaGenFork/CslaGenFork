fa32e3d91370546cd36f973653f9356fe2ccd38c
Tiago Freitas Leal <tfreitasleal@gmail.com>
2012-04-09 01:49:37 +0000

    Tag Rules sampls release 1.1.0

---
+++ tags/Rules sample v.1.1.0/CslaGenFork.Rules-4.3.10.SL4/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CslaGenFork.Rules-4.3.10 SL4")]
+[assembly: AssemblyDescription("Business and Authorization Rules library for CSLA 4.3.10 and Silverlight 4")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CslaGenFork Project")]
+[assembly: AssemblyProduct("CslaGenFork.Rules for CSLA v.4.3.10 and Silverlight 4")]
+[assembly: AssemblyCopyright("Copyright © CslaGenFork Project, 2012")]
+[assembly: AssemblyTrademark("All Rights Reserved.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("93d384f1-a539-404f-a5f0-ff4b343c34a3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
