1a405b2bb1c77c9ebbd79986789ace59c13dd91f
Tiago Freitas Leal <tfreitasleal@gmail.com>
2013-01-26 17:48:56 +0000

    DeepLoad sample v.3.0.0 - update version and small changes Work Item #1406

--- trunk/Samples/DeepLoad/GlobalAssemblyInfo.cs
+++ trunk/Samples/DeepLoad/GlobalAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © CslaGen Project, 2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
