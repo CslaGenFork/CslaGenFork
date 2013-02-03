d0f79bd786142dc5643c6f1f7c693466c7c2af7d
Tiago Freitas Leal <tfreitasleal@gmail.com>
2013-02-03 10:24:12 +0000

    DAL-DTO business projects were missing OnDeserialized region Work Item #1415

--- trunk/Samples/DeepLoad/DAL-DTO/ParentLoadROSoftDelete.Business/ERCLevel/F11_CityRoadColl.cs
+++ trunk/Samples/DeepLoad/DAL-DTO/ParentLoadROSoftDelete.Business/ERCLevel/F11_CityRoadColl.cs
@@ -1,9 +1,21 @@
-using ParentLoadROSoftDelete.Business;
-
 namespace ParentLoadROSoftDelete.Business.ERCLevel
 {
     public partial class F11_CityRoadColl
     {
+
+        #region OnDeserialized actions
+
+        /*/// <summary>
+        /// This method is called on a newly deserialized object
+        /// after deserialization is complete.
+        /// </summary>
+        protected override void OnDeserialized()
+        {
+            base.OnDeserialized();
+            // add your custom OnDeserialized actions here.
+        }*/
+
+        #endregion
 
         #region Pseudo Event Handlers
 
