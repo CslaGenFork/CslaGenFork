5cd00793a1f90d70d973b68f02f97f4bd25e7018
Tiago Freitas Leal <tfreitasleal@gmail.com>
2013-03-21 14:18:07 +0000

    Update version to 4.3RC

--- trunk/Solutions/CslaGenFork/Properties/AssemblyInfo.cs
+++ trunk/Solutions/CslaGenFork/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CslaGenFork Project")]
 [assembly: AssemblyProduct("Csla Generator Fork")]
-[assembly: AssemblyCopyright("Copyright © CslaGen Project 2007, 2009\r\nCopyright © Tiago Freitas Leal 2009, 2010, 2011, 2012")]
+[assembly: AssemblyCopyright("Copyright © CslaGen Project 2007, 2009\r\nCopyright © Tiago Freitas Leal 2009, 2013")]
 [assembly: AssemblyTrademark("All Rights Reserved.")]
 [assembly: AssemblyCulture("")]
 
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("4.0.3.*")]
+[assembly: AssemblyVersion("4.3.0.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
@@ -57,6 +57,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
-[assembly: AssemblyFileVersionAttribute("4.0.17")]
+[assembly: AssemblyFileVersionAttribute("4.3.0")]
 [assembly: GuidAttribute("5c019c4f-d2ab-40dd-9860-70bd603b6017")]
 [assembly: ComVisibleAttribute(false)]
