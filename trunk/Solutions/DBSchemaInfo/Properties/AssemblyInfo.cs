81292d5b2a0dda759e4994014485c7c2e50eff4f
Tiago Freitas Leal <tfreitasleal@gmail.com>
2013-04-01 09:58:11 +0000

    Update DBSchemaInfo to version 1.1.0.0 - targets NET 4.0 Client Profile - no MySql dependency For types Time, Date, DateTime2, DateTimeOffset - add to mapppers SqlDbType to System.Type and to DbType - add to GetCondensedDataType method Work Item #1508

--- trunk/Solutions/DBSchemaInfo/Properties/AssemblyInfo.cs
+++ trunk/Solutions/DBSchemaInfo/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DBSchemaInfo.MsSQL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Interface and adapter for SQL Sever DB schema.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CslaGenFork Project")]
 [assembly: AssemblyProduct("DBSchemaInfo.MsSQL")]
 [assembly: AssemblyCopyright("Copyright © CslaGen Project, 2007 && Tiago Freitas Leal, 2011")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("All Rights Reserved.")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7b200c13-fe4c-45be-9a36-83eb5917b00c")]
+//[assembly: Guid("7b200c13-fe4c-45be-9a36-83eb5917b00c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.5")]
-[assembly: AssemblyFileVersion("1.0.0.5")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
