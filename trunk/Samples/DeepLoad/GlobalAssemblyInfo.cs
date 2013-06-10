33b34c9e6fc6dadde0e0a1201e6e94f180beb7d1
Tiago Freitas Leal <tfreitasleal@gmail.com>
2013-06-10 23:38:53 +0000

    Add unit tests for DeepLoad sample Work Item #1569

--- trunk/Samples/DeepLoad/GlobalAssemblyInfo.cs
+++ trunk/Samples/DeepLoad/GlobalAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © CslaGen Project, 2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
