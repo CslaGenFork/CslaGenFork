e03ffe4ccb8d3cf28c629157ae623f618d28560a
tiago_cp <SND\tiago_cp>
2014-08-11 23:13:26 +0000

    Global Settings: encodings and overwrite Extended file Work Item #1775

--- trunk/Solutions/CslaGenFork/Controls/GlobalSettings.cs
+++ trunk/Solutions/CslaGenFork/Controls/GlobalSettings.cs
@@ -1,9 +1,7 @@
 using System;
+using System.Text;
 using System.Windows.Forms;
 using CslaGenerator.Metadata;
-using System.IO;
-using System.Xml.Serialization;
-using CslaGenerator.Util;
 using WeifenLuo.WinFormsUI.Docking;
 
 namespace CslaGenerator.Controls
@@ -62,354 +60,91 @@ namespace CslaGenerator.Controls
 
         #endregion
 
-        private GenerationParameters _genParams;
-        private ProjectParameters _projParams;
+        private GlobalParameters _globalParams;
+
+        public GlobalParameters GlobalParameters
+        {
+            get { return Controller.GlobalParameters; }
+            set
+            {
+                if (value != null)
+                    Controller.GlobalParameters = value;
+            }
+        }
 
         internal GlobalSettings()
         {
             InitializeComponent();
-            FillComboBox(cboOutputLanguage, typeof (CodeLanguage));
-            FillComboBox(cboTarget, typeof (TargetFramework));
-            FillComboBox(cboGenerateAuthorization, typeof (AuthorizationLevel));
-            FillComboBox(cboHeaderVerbosity, typeof (HeaderVerbosity));
-            FillComboBox(cboTransactionType, typeof (TransactionType));
-            FillComboBox(cboPersistenceType, typeof (PersistenceType));
-            FillComboBox(cboCreateTimestampPropertyMode, typeof (PropertyDeclaration));
-            FillComboBox(cboCreateReadOnlyObjectsPropertyMode, typeof (PropertyDeclaration));
-            FillComboBox(cboInlineQueries, typeof(UseInlineQueries));
+
+            FillEncodingComboBox(cboCodeEncoding);
+            FillEncodingComboBox(cboSprocEncoding);
         }
 
-        private void FillComboBox(ComboBox cbo, Type enumType)
+        private void FillEncodingComboBox(ComboBox cbo)
         {
-            foreach (var str in Enum.GetNames(enumType))
+            var encodings = Encoding.GetEncodings();
+            foreach (var encoding in encodings)
             {
-                cbo.Items.Add(str);
+                cbo.Items.Add(encoding.Name);
             }
-            cbo.Tag = enumType;
-            cbo.DataBindings[0].Parse += EnumDropDownParse;
+            cbo.Tag = typeof (string);
         }
 
-        private void EnumDropDownParse(object sender, ConvertEventArgs e)
+        private GeneratorController Controller
         {
-            var binding = (Binding) sender;
-            var cbo = (ComboBox) binding.Control;
-            if (cbo != null)
-            {
-                var t = (Type) cbo.Tag;
-                e.Value = Enum.Parse(t, e.Value.ToString());
-            }
-        }
-
-        private CslaGeneratorUnit project
-        {
-            get { return GeneratorController.Current.CurrentUnit; }
+            get { return GeneratorController.Current; }
         }
 
         internal void LoadInfo()
         {
-            _genParams = project.GenerationParams.Clone();
-            _projParams = project.Params.Clone();
-            generationParametersBindingSource.DataSource = _genParams;
-            projectParametersBindingSource.DataSource = _projParams;
-
-            NotYetImplemented();
-        }
-
-        private void NotYetImplemented()
-        {
-            txtIntSoftDelete.Enabled = false;
+            _globalParams = Controller.GlobalParameters.Clone();
+            globalParametersBindingSource.DataSource = _globalParams;
         }
 
         internal void SaveInfo()
         {
-            if (_genParams.Dirty)
-                project.GenerationParams = _genParams.Clone();
-            if (_projParams.Dirty)
-                project.Params = _projParams.Clone();
             LoadInfo();
-            project.GenerationParams.ResetAckSilverlightPublicPropertyInfo();
         }
 
-        private void CmdApplyClick(object sender, EventArgs e)
+        private void CmdSaveClick(object sender, EventArgs e)
         {
-            if (!ValidateOptions())
-                return;
+            if (IsDirty)
+                Controller.GlobalParameters = _globalParams.Clone();
 
-            var confirm = DialogResult.No;
-            if (!(_projParams.SpGeneralPrefix.Equals(project.Params.SpGeneralPrefix) &&
-                _projParams.SpGetPrefix.Equals(project.Params.SpGetPrefix) &&
-                _projParams.SpAddPrefix.Equals(project.Params.SpAddPrefix) &&
-                _projParams.SpUpdatePrefix.Equals(project.Params.SpUpdatePrefix) &&
-                _projParams.SpDeletePrefix.Equals(project.Params.SpDeletePrefix) &&
-                _projParams.SpGeneralSuffix.Equals(project.Params.SpGeneralSuffix) &&
-                _projParams.SpGetSuffix.Equals(project.Params.SpGetSuffix) &&
-                _projParams.SpAddSuffix.Equals(project.Params.SpAddSuffix) &&
-                _projParams.SpUpdateSuffix.Equals(project.Params.SpUpdateSuffix) &&
-                _projParams.SpDeleteSuffix.Equals(project.Params.SpDeleteSuffix)))
-            {
-                confirm = MessageBox.Show(@"Your SP headings have changed. Do you wish to update your business objects to reflect these changes?", @"SP Naming", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            }
+            Controller.GlobalParameters.SaveGlobalParameters();
             SaveInfo();
-            GeneratorController.Current.ReloadPropertyGrid();
-            if (confirm == DialogResult.Yes)
-            {
-                foreach (var info in project.CslaObjects)
-                {
-                    info.SetProcedureNames();
-                }
-            }
         }
 
-        private void CmdUndoClick(object sender, EventArgs e)
+        private void CmdCancelClick(object sender, EventArgs e)
         {
             LoadInfo();
-        }
-
-        private void CmdExportClick(object sender, EventArgs e)
-        {
-            using (var fileSave = new OpenFileDialog())
-            {
-                fileSave.Title = @"Export project settings - Select an existing file or type a new file name";
-                fileSave.Filter = @"CSLA Gen files (*.xml) | *.xml";
-                fileSave.DefaultExt = "xml";
-                fileSave.Multiselect = false;
-                fileSave.CheckFileExists = false;
-                fileSave.CheckPathExists = true;
-                fileSave.AddExtension = true;
-                DialogResult result = fileSave.ShowDialog(this);
-                if (result != DialogResult.OK)
-                    return;
-
-                Application.DoEvents();
-                ExportParams(fileSave.FileName);
-            }
-        }
-
-        private void CmdGetDefaultClick(object sender, EventArgs e)
-        {
-            ImportParams(ConfigTools.DefaultXml);
-        }
-
-        private void CmdSetDefaultClick(object sender, EventArgs e)
-        {
-            ExportParams(ConfigTools.DefaultXml);
         }
 
         private void CmdResetToFactoryClick(object sender, EventArgs e)
         {
-            ImportParams(Application.StartupPath + @"\Factory.xml");
+            Controller.GlobalParameters.LoadGlobalParameters(true);
+            LoadInfo();
         }
 
-        private void CmdImportClick(object sender, EventArgs e)
-        {
-            using (var fileLoad = new OpenFileDialog())
-            {
-                fileLoad.Title = @"Import project settings - Select an existing file";
-                fileLoad.Filter = @"CSLA Gen files (*.xml) | *.xml";
-                fileLoad.DefaultExt = "xml";
-                fileLoad.CheckFileExists = true;
-                fileLoad.CheckPathExists = true;
-                fileLoad.Multiselect = false;
-                var result = fileLoad.ShowDialog(this);
-                if (result != DialogResult.OK)
-                    return;
-
-                Application.DoEvents();
-                ImportParams(fileLoad.FileName);
-            }
-        }
-
-        private void ImportParams(string filename)
-        {
-            var currentCursor = Cursor.Current;
-            Cursor.Current = Cursors.WaitCursor;
-            try
-            {
-                Cursor.Current = Cursors.WaitCursor;
-                CslaGeneratorUnit unit;
-                using (var fs = File.Open(filename, FileMode.Open, FileAccess.Read))
-                {
-                    var s = new XmlSerializer(typeof(CslaGeneratorUnit));
-                    unit = (CslaGeneratorUnit)s.Deserialize(fs);
-                }
-                if (unit != null)
-                {
-                    project.Params = unit.Params;
-                    project.GenerationParams = unit.GenerationParams;
-                    LoadInfo();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(@"An error occurred while trying to import: " + Environment.NewLine + ex.Message, @"Import Error");
-            }
-            finally
-            {
-                Cursor.Current = currentCursor;
-            }
-        }
-
-        internal void ExportParams(string fileName)
-        {
-            if (!ApplyGlobalSettings())
-                return;
-
-            var privateUnit = new CslaGeneratorUnit();
-            privateUnit.GenerationParams = _genParams.Clone();
-            privateUnit.Params = _projParams.Clone();
-            FileStream fs = null;
-            var tempFile = Path.GetTempPath() + Guid.NewGuid() + ".cslagenerator";
-            var success = false;
-            try
-            {
-                Cursor.Current = Cursors.WaitCursor;
-                fs = File.Open(tempFile, FileMode.Create);
-                var s = new XmlSerializer(typeof(CslaGeneratorUnit));
-                s.Serialize(fs, privateUnit);
-                success = true;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(@"An error occurred while trying to export: " + Environment.NewLine + ex.Message, @"Export Error");
-            }
-            finally
-            {
-                Cursor.Current = Cursors.Default;
-                if (fs != null)
-                    fs.Close();
-            }
-
-            if (success)
-            {
-                File.Delete(fileName);
-                File.Move(tempFile, fileName);
-            }
-        }
-
-        private void GenerationParametersBindingSourceCurrentItemChanged(object sender, EventArgs e)
+        private void GlobalParametersBindingSourceCurrentItemChanged(object sender, EventArgs e)
         {
             if (IsDirty)
                 TabText = @"Global Settings *";
             else
                 TabText = @"Global Settings";
-
-            if (UseCsla4)
-            {
-                chkActiveObjects.Checked = false;
-                chkSynchronous.Enabled = !_genParams.ForceSync;
-                chkAsynchronous.Enabled = !_genParams.ForceAsync;
-                cboGenerateAuthorization.Enabled = true;
-                chkUsesCslaAuthorizationProvider.Enabled =
-                    (_genParams.GenerateAuthorization != AuthorizationLevel.None &&
-                    _genParams.GenerateAuthorization != AuthorizationLevel.Custom);
-            }
-            else
-            {
-                chkWinForms.Checked = true;
-                chkWPF.Checked = false;
-                chkSilverlight.Checked = false;
-                chkSilverlightUseServices.Checked = false;
-                chkSynchronous.Checked = true;
-                chkAsynchronous.Checked = false;
-
-                chkSynchronous.Enabled = false;
-                chkAsynchronous.Enabled = false;
-                cboGenerateAuthorization.Enabled = false;
-                chkUsesCslaAuthorizationProvider.Enabled = false;
-            }
-            chkGenerateDTO.Enabled = UseDal;
-            chkGenerateDalInterface.Enabled = UseDal;
-            chkGenerateDalObject.Enabled = UseDal;
-            txtDalInterfaceNamespace.Enabled = UseDal;
-            txtDalObjectNamespace.Enabled = UseDal;
-            chkGenerateDatabaseClass.Enabled = !UseDal;
-            txtDalName.Enabled = UseDal;
-
-            txtDatabase.Enabled = !UseCsla4;
-            txtDatabaseConnection.Enabled = UseCsla4;
-            chkWinForms.Enabled = UseCsla4;
-            chkWPF.Enabled = UseCsla4;
-            chkSilverlight.Enabled = UseCsla4;
-            chkSilverlightUseServices.Enabled = UseCsla4;
-            chkGenerateQueriesWithSchema.Enabled = UseCsla4;
-            chkUsePublicPropertyInfo.Enabled = UseCsla4;
-            chkUseChildFactory.Enabled = UseCsla4;
-            chkUseBypassPropertyChecks.Enabled = UseCsla4;
-            chkActiveObjects.Enabled = !UseCsla4;
-
-            chkSpOneFile.Enabled = _genParams.GenerateSprocs;
         }
 
         internal bool IsDirty
         {
-            get { return (_genParams.Dirty || _projParams.Dirty); }
+            get { return _globalParams.Dirty; }
         }
 
-        private bool UseCsla4
-        {
-            get { return (_genParams.UseCsla4); }
-        }
-
-        private bool UseDal
-        {
-            get { return (_genParams.UseDal); }
-        }
-
-        internal bool ApplyGlobalSettings()
+        internal void ForceSaveGlobalSettings()
         {
             if (IsDirty)
             {
-                var result = MessageBox.Show(@"There are unsaved changes in the Global Settings tab. Would you like to apply them now?", @"CslaGenerator", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
-                switch (result)
-                {
-                    case DialogResult.Yes:
-                        cmdApply.PerformClick();
-                        break;
-                    case DialogResult.Cancel:
-                        return false;
-                }
+                cmdSave.PerformClick();
             }
-            return true;
-        }
-
-        internal bool ValidateOptions()
-        {
-            var result = true;
-
-            if (!_genParams.GenerateWinForms && !_genParams.GenerateWPF && _genParams.SilverlightUsingServices && UseDal)
-            {
-                result = false;
-                MessageBox.Show(@"Must select at least one of these options:" + Environment.NewLine +
-                                @"- Windows Forms" + Environment.NewLine +
-                                @"- WPF",
-                                @"CslaGenerator", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else if (!_genParams.GenerateWinForms && !_genParams.GenerateWPF && _genParams.GenerateSilverlight4)
-            {
-                result = false;
-                MessageBox.Show(@"Must select at least one of these options:" + Environment.NewLine +
-                                @"- Windows Forms" + Environment.NewLine +
-                                @"- WPF",
-                                @"CslaGenerator", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else if (!_genParams.GenerateWinForms && !_genParams.GenerateWPF && !_genParams.SilverlightUsingServices)
-            {
-                result = false;
-                MessageBox.Show(@"Must select at least one of these options:" + Environment.NewLine +
-                                @"- Windows Forms" + Environment.NewLine +
-                                @"- WPF" + Environment.NewLine +
-                                @"- Silverlight using services",
-                                @"CslaGenerator", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-
-            if (!_genParams.GenerateAsynchronous && !_genParams.GenerateSynchronous &&
-                (_genParams.GenerateWinForms || _genParams.GenerateWPF))
-            {
-                result = false;
-                MessageBox.Show(@"Must select either Synchronous or Asynchronous server methods.", @"CslaGenerator", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-
-            return result;
         }
 
         #region Manage state
@@ -455,6 +190,19 @@ namespace CslaGenerator.Controls
                     break;
                 }
             }
+
+            if (mainTabPage != null)
+            {
+                GeneratorController.Current.CurrentUnitLayout.GlobalSettingsMainTab = mainTabPage.Name;
+                GeneratorController.Current.CurrentUnitLayout.GlobalSettingsMainTabHidden = false;
+            }
+            else
+                GeneratorController.Current.CurrentUnitLayout.GlobalSettingsMainTab = string.Empty;
+
+            if (subTabPage != null)
+                GeneratorController.Current.CurrentUnitLayout.GlobalSettingsSubTab = subTabPage.Name;
+            else
+                GeneratorController.Current.CurrentUnitLayout.GlobalSettingsSubTab = string.Empty;
         }
 
         internal void SetState()
@@ -472,7 +220,7 @@ namespace CslaGenerator.Controls
                         for (var index1 = 0; index1 < tabControl1.TabPages.Count; index1++)
                         {
                             var tabPage1 = tabControl1.TabPages[index1];
-                            if (tabPage1.Name == GeneratorController.Current.CurrentUnitLayout.ProjectPropertiesMainTab)
+                            if (tabPage1.Name == GeneratorController.Current.CurrentUnitLayout.GlobalSettingsMainTab)
                             {
                                 tabControl1.SelectedIndex = index1;
                                 tabPage1.Focus();
@@ -489,7 +237,7 @@ namespace CslaGenerator.Controls
                                             for (var index2 = 0; index2 < tabControl2.TabPages.Count; index2++)
                                             {
                                                 var tabPage2 = tabControl2.TabPages[index2];
-                                                if (tabPage2.Name == GeneratorController.Current.CurrentUnitLayout.ProjectPropertiesSubTab)
+                                                if (tabPage2.Name == GeneratorController.Current.CurrentUnitLayout.GlobalSettingsSubTab)
                                                 {
                                                     tabControl2.SelectedIndex = index2;
                                                     tabPage2.Focus();
