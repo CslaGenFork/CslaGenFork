19e8d640733c219e24b9608043d1225c4578bbc1
tiago_cp <SND\tiago_cp>
2015-06-23 21:30:28 +0000

    Can't load schema information for a database that contains a stored procedure with a TVP parameter. Work Item #1892

--- trunk/Solutions/DBSchemaInfo/MsSql/SqlTypeMapper.cs
+++ trunk/Solutions/DBSchemaInfo/MsSql/SqlTypeMapper.cs
@@ -3,21 +3,21 @@ using System.Data;
 
 namespace DBSchemaInfo.MsSql
 {
-    class SqlTypeMapper
+    internal class SqlTypeMapper
     {
         public static Type GetManagedType(SqlDbType sqlDbType)
         {
             switch (sqlDbType)
             {
                 case SqlDbType.BigInt:
-                    return typeof(Int64);
+                    return typeof (Int64);
                 case SqlDbType.Binary:
                 case SqlDbType.Image:
                 case SqlDbType.Timestamp:
                 case SqlDbType.VarBinary:
-                    return typeof(byte[]);
+                    return typeof (byte[]);
                 case SqlDbType.Bit:
-                    return typeof(bool);
+                    return typeof (bool);
                 case SqlDbType.Char:
                 case SqlDbType.NChar:
                 case SqlDbType.NText:
@@ -25,36 +25,36 @@ namespace DBSchemaInfo.MsSql
                 case SqlDbType.Text:
                 case SqlDbType.VarChar:
                 case SqlDbType.Xml:
-                    return typeof(string);
+                    return typeof (string);
                 case SqlDbType.Time:
-                    return typeof(TimeSpan);
+                    return typeof (TimeSpan);
                 case SqlDbType.DateTimeOffset:
-                    return typeof(DateTimeOffset);
+                    return typeof (DateTimeOffset);
                 case SqlDbType.Date:
                 case SqlDbType.DateTime2:
                 case SqlDbType.DateTime:
                 case SqlDbType.SmallDateTime:
-                    return typeof(DateTime);
+                    return typeof (DateTime);
                 case SqlDbType.Decimal:
                 case SqlDbType.Money:
                 case SqlDbType.SmallMoney:
-                    return typeof(decimal);
+                    return typeof (decimal);
                 case SqlDbType.Float:
-                    return typeof(double);
+                    return typeof (double);
                 case SqlDbType.Int:
-                    return typeof(int);
+                    return typeof (int);
                 case SqlDbType.Real:
-                    return typeof(Single);
+                    return typeof (Single);
                 case SqlDbType.UniqueIdentifier:
-                    return typeof(Guid);
+                    return typeof (Guid);
                 case SqlDbType.SmallInt:
-                    return typeof(Int16);
+                    return typeof (Int16);
                 case SqlDbType.TinyInt:
-                    return typeof(byte);
+                    return typeof (byte);
                 case SqlDbType.Variant:
                 case SqlDbType.Udt:
                 default:
-                    return typeof(object);
+                    return typeof (object);
             }
         }
 
@@ -119,9 +119,9 @@ namespace DBSchemaInfo.MsSql
             var tempType = type;
             if (tempType == "numeric")
                 tempType = "decimal";
-            if (tempType == "sql_variant")
+            if (tempType == "sql_variant" || tempType == "table type")
                 tempType = "variant";
-            return (SqlDbType)Enum.Parse(typeof(SqlDbType), tempType, true);
+            return (SqlDbType) Enum.Parse(typeof (SqlDbType), tempType, true);
         }
 
         public static string GetCondensedDataType(string nativeType, long columnLength, int columnScale)
