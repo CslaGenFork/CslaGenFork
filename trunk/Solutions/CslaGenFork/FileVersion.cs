9bd2832778ca272523c5dec55560b78f38989a63
Tiago Freitas Leal <tfreitasleal@gmail.com>
2015-07-14 20:49:42 +0000

    Project file format conversion is broken. Work Item #1915

--- trunk/Solutions/CslaGenFork/FileVersion.cs
+++ trunk/Solutions/CslaGenFork/FileVersion.cs
@@ -1,47 +1,67 @@
-﻿
+﻿using System;
+
 namespace CslaGenerator
 {
-    internal class FileVersion
-    {
-        internal const string CurrentFileVersion = "4.0.3";
+	internal class FileVersion
+	{
+		internal const string CurrentFileVersion = "4.0.3";
+		internal static string FileVersionFound;
 
-        internal static string ExtractFileVersion(string[] fileLines)
-        {
-            var fileVersion = string.Empty;
-            foreach (var line in fileLines)
-            {
-                var lineStart = line.IndexOf(@"<FileVersion>");
-                if (lineStart != -1)
-                {
-                    lineStart += 13;
-                    var length = line.IndexOf(@"</FileVersion>") - lineStart;
-                    fileVersion = line.Substring(lineStart, length);
-                    break;
-                }
-            }
-            return fileVersion;
-        }
+		internal static string ExtractFileVersion(string[] fileLines)
+		{
+			var fileVersion = string.Empty;
+			foreach (var line in fileLines)
+			{
+				var lineStart = line.IndexOf(@"<FileVersion>");
+				if (lineStart != -1)
+				{
+					lineStart += 13;
+					var length = line.IndexOf(@"</FileVersion>") - lineStart;
+					fileVersion = line.Substring(lineStart, length);
+					break;
+				}
+			}
 
-        internal static string[] HandleFileVersion(string fileVersion, string[] fileLines)
-        {
-            if (fileVersion == "4.0.2")
-                return ConvertFrom402To403(fileLines);
+			FileVersionFound = fileVersion;
+			return fileVersion;
+		}
 
-            return fileLines;
-        }
+		internal static int ConvertDotFileVersionToInt(string fileVersion)
+		{
+			var noDotsFileVersion = fileVersion.Replace(".", string.Empty);
+			int convertedFileVersion;
+			try
+			{
+				convertedFileVersion = Convert.ToInt32(noDotsFileVersion);
+			}
+			catch (Exception)
+			{
+				return 0;
+			}
 
-        internal static string[] ConvertFrom402To403(string[] fileLines)
-        {
-            for (int index = 0; index < fileLines.Length; index++)
-            {
-                fileLines[index] = fileLines[index].Replace(@"<RelationType>MultipleToMultiple</RelationType>",
-                                                            @"<RelationType>ManyToMany</RelationType>");
-                fileLines[index] = fileLines[index].Replace(@"<RelationType>OneToMultiple</RelationType>",
-                                                            @"<RelationType>OneToMany</RelationType>");
-                fileLines[index] = fileLines[index].Replace(@"<FileVersion>4.0.2</FileVersion>",
-                                                            @"<FileVersion>4.0.3</FileVersion>");
-            }
-            return fileLines;
-        }
-    }
+			return convertedFileVersion;
+		}
+
+		internal static string[] HandleFileVersion(int intFileVersion, string[] fileLines)
+		{
+			if (intFileVersion < 403)
+				return ConvertFrom402To403(fileLines);
+
+			return fileLines;
+		}
+
+		internal static string[] ConvertFrom402To403(string[] fileLines)
+		{
+			for (var index = 0; index < fileLines.Length; index++)
+			{
+				fileLines[index] = fileLines[index].Replace(@"<RelationType>MultipleToMultiple</RelationType>",
+					@"<RelationType>ManyToMany</RelationType>");
+				fileLines[index] = fileLines[index].Replace(@"<RelationType>OneToMultiple</RelationType>",
+					@"<RelationType>OneToMany</RelationType>");
+				fileLines[index] = fileLines[index].Replace(@"<FileVersion>" + FileVersionFound + "</FileVersion>",
+					@"<FileVersion>4.0.3</FileVersion>");
+			}
+			return fileLines;
+		}
+	}
 }
