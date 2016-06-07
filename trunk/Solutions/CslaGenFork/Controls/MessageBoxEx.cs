49d014e095cfaeb9adb3e07fa5b0ca74464b9ca9
tiago_cp <SND\tiago_cp>
2016-06-07 20:48:07 +0000

    Add MessageBoxEx control.

---
+++ trunk/Solutions/CslaGenFork/Controls/MessageBoxEx.cs
@@ -0,0 +1,385 @@
+﻿/************************************************************************************************
+ *                                                                                              *
+ * Author: Max Kleyzit.                                                                         *
+ * Developed for StresStimulus, free Fiddler extension for load testing of web applications     *
+ * http://stresstimulus.stimulustechnology.com/                                                 *
+ *                                                                                              *
+ * Changed by Tiago Freitas Leal, 07 Jun 2016                                                   *
+ * Cast DialogResult to MessageBoxResult so MessageBoxResult is the single result point         *
+ * (no need to check for results on DialogResult)                                               *
+ *                                                                                              *
+ * Subject to The Code Project Open License (CPOL) 1.02                                         *
+ * http://www.codeproject.com/info/cpol10.aspx                                                  *
+ *                                                                                              *
+ ***********************************************************************************************/
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CslaGenerator.Controls
+{
+    /// <summary>
+    /// A customizable Dialog box with 3 buttons, custom icon, and checkbox.
+    /// </summary>
+    partial class MessageBox : Form
+    {
+        #region Constants
+
+        private const int FormYMargin = 10;
+        private const int FormXMargin = 16;
+        private const int ButtonSpace = 5;
+        private const int CheckboxSpace = 15;
+        private const int TextYMargin = 30;
+
+        #endregion
+
+        #region Fields
+
+        /// <summary>
+        /// The icon to paint.
+        /// </summary>
+        private readonly Icon _systemIcon;
+
+        /// <summary>
+        /// Min set width.
+        /// </summary>
+        private int _minWidth;
+
+        /// <summary>
+        /// Min set height.
+        /// </summary>
+        private int _minHeight;
+
+        /// <summary>
+        /// The min required width of the button and checkbox row. Sum of button widths + checkbox width + margins.
+        /// </summary>
+        private int _minButtonRowWidth;
+
+        #endregion
+
+        #region Contructors
+
+        /// <summary>
+        /// Create a new instance of the dialog box with a message and title.
+        /// </summary>
+        /// <param name="message">Message text.</param>
+        /// <param name="title">Dialog Box title.</param>
+        public MessageBox(string message, string title)
+            : this(message, title, MessageBoxIcon.None)
+        {
+        }
+
+        /// <summary>
+        /// Create a new instance of the dialog box with a message and title and a standard windows messagebox icon.
+        /// </summary>
+        /// <param name="message">Message text.</param>
+        /// <param name="title">Dialog Box title.</param>
+        /// <param name="icon">Standard system messagebox icon.</param>
+        public MessageBox(string message, string title, MessageBoxIcon icon)
+            : this(message, title, GetMessageBoxIcon(icon))
+        {
+        }
+
+        /// <summary>
+        /// Create a new instance of the dialog box with a message and title and a custom windows icon.
+        /// </summary>
+        /// <param name="message">Message text.</param>
+        /// <param name="title">Dialog Box title.</param>
+        /// <param name="icon">Custom icon.</param>
+        public MessageBox(string message, string title, Icon icon)
+        {
+            InitializeComponent();
+
+            messageLabel.Text = message;
+            Text = title;
+
+            _systemIcon = icon;
+
+            if (_systemIcon == null)
+                messageLabel.Location = new Point(FormXMargin, FormYMargin);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Get system icon for MessageBoxIcon.
+        /// </summary>
+        /// <param name="icon">The MessageBoxIcon value.</param>
+        /// <returns>SystemIcon type Icon.</returns>
+        private static Icon GetMessageBoxIcon(MessageBoxIcon icon)
+        {
+            switch (icon)
+            {
+                case MessageBoxIcon.Asterisk:
+                    return SystemIcons.Asterisk;
+                case MessageBoxIcon.Error:
+                    return SystemIcons.Error;
+                case MessageBoxIcon.Exclamation:
+                    return SystemIcons.Exclamation;
+                case MessageBoxIcon.Question:
+                    return SystemIcons.Question;
+                default:
+                    return null;
+            }
+        }
+
+        #region Setup API
+
+        /// <summary>
+        /// Sets the min size of the dialog box. If the text or button row needs more size then the dialog box will size to fit the text.
+        /// </summary>
+        /// <param name="width">Min width value.</param>
+        /// <param name="height">Min height value.</param>
+        public void SetMinSize(int width, int height)
+        {
+            _minWidth = width;
+            _minHeight = height;
+        }
+
+        /// <summary>
+        /// Create up to 3 buttons with no DialogResult values.
+        /// </summary>
+        /// <param name="names">Array of button names. Must of length 1-3.</param>
+        public void SetButtons(params string[] names)
+        {
+            var dialogResults = new DialogResult[names.Length];
+            for (var i = 0; i < names.Length; i++)
+                dialogResults[i] = DialogResult.None;
+
+            SetButtons(names, dialogResults);
+        }
+
+        /// <summary>
+        /// Create up to 3 buttons with given DialogResult values.
+        /// </summary>
+        /// <param name="names">Array of button names. Must of length 1-3.</param>
+        /// <param name="results">Array of DialogResult values. Must be same length as names.</param>
+        public void SetButtons(string[] names, DialogResult[] results)
+        {
+            SetButtons(names, results, 1);
+        }
+
+        /// <summary>
+        /// Create up to 3 buttons with given DialogResult values.
+        /// </summary>
+        /// <param name="names">Array of button names. Must of length 1-3.</param>
+        /// <param name="results">Array of DialogResult values. Must be same length as names.</param>
+        /// <param name="defaultButton">Default Button number. Must be 1-3.</param>
+        public void SetButtons(string[] names, DialogResult[] results, int defaultButton)
+        {
+            if (names == null)
+                throw new ArgumentNullException(nameof(names), @"Button Text is null");
+
+            int count = names.Length;
+
+            if (count < 1 || count > 3)
+                throw new ArgumentException("Invalid number of buttons. Must be between 1 and 3.");
+
+            //---- Set Button 1
+            _minButtonRowWidth += SetButtonParams(button1, names[0], defaultButton == 1 ? 1 : 2, results[0]);
+
+            //---- Set Button 2
+            if (count > 1)
+            {
+                _minButtonRowWidth += SetButtonParams(button2, names[1], defaultButton == 2 ? 1 : 3,
+                    results[1]) + ButtonSpace;
+            }
+
+            //---- Set Button 3
+            if (count > 2)
+            {
+                _minButtonRowWidth += SetButtonParams(button3, names[2], defaultButton == 3 ? 1 : 4,
+                    results[2]) + ButtonSpace;
+            }
+        }
+
+        /// <summary>
+        /// Sets button text and returns the width.
+        /// </summary>
+        /// <param name="button">Button object.</param>
+        /// <param name="text">Text of the button.</param>
+        /// <param name="tabIndex">TabIndex of the button.</param>
+        /// <param name="dialogResult">DialogResult of the button.</param>
+        /// <returns>Width of the button.</returns>
+        private static int SetButtonParams(Button button, string text, int tabIndex, DialogResult dialogResult)
+        {
+            button.Text = text;
+            button.Visible = true;
+            button.DialogResult = dialogResult;
+            button.TabIndex = tabIndex;
+
+            return button.Size.Width;
+        }
+
+        /// <summary>
+        /// Enables the checkbox. By default the checkbox is unchecked.
+        /// </summary>
+        /// <param name="text">Text of the checkbox.</param>
+        public void SetCheckbox(string text)
+        {
+            SetCheckbox(text, false);
+        }
+
+        /// <summary>
+        /// Enables the checkbox and the default checked state.
+        /// </summary>
+        /// <param name="text">Text of the checkbox.</param>
+        /// <param name="checkedState">Default checked state of the box.</param>
+        public void SetCheckbox(string text, bool checkedState)
+        {
+            checkBox.Visible = true;
+            checkBox.Text = text;
+            checkBox.Checked = checkedState;
+            _minButtonRowWidth += checkBox.Size.Width + CheckboxSpace;
+        }
+
+        #endregion
+
+        #region Sizes and Locations
+
+        private void DialogBox_Load(object sender, EventArgs e)
+        {
+            if (!button1.Visible)
+                SetButtons(new[] {"OK"}, new[] {DialogResult.OK});
+
+            _minButtonRowWidth += 2*FormXMargin; //add margin to the ends
+
+            SetDialogSize();
+
+            SetButtonRowLocations();
+        }
+
+        /// <summary>
+        /// Auto fits the dialog box to fit the text and the buttons.
+        /// </summary>
+        private void SetDialogSize()
+        {
+            var requiredWidth = messageLabel.Location.X + messageLabel.Size.Width + FormXMargin;
+            requiredWidth = requiredWidth > _minButtonRowWidth ? requiredWidth : _minButtonRowWidth;
+
+            var requiredHeight = messageLabel.Location.Y + messageLabel.Size.Height -
+                                 button2.Location.Y + ClientSize.Height + TextYMargin;
+
+            var minSetWidth = ClientSize.Width > _minWidth ? ClientSize.Width : _minWidth;
+            var minSetHeight = ClientSize.Height > _minHeight ? ClientSize.Height : _minHeight;
+
+            var size = new Size();
+            size.Width = requiredWidth > minSetWidth ? requiredWidth : minSetWidth;
+            size.Height = requiredHeight > minSetHeight ? requiredHeight : minSetHeight;
+            ClientSize = size;
+        }
+
+        /// <summary>
+        /// Sets the buttons and checkboxe location.
+        /// </summary>
+        private void SetButtonRowLocations()
+        {
+            var formWidth = ClientRectangle.Width;
+
+            var x = formWidth - FormXMargin;
+            var y = button1.Location.Y;
+
+            if (button3.Visible)
+            {
+                x -= button3.Size.Width;
+                button3.Location = new Point(x, y);
+                x -= ButtonSpace;
+            }
+
+            if (button2.Visible)
+            {
+                x -= button2.Size.Width;
+                button2.Location = new Point(x, y);
+                x -= ButtonSpace;
+            }
+
+            x -= button1.Size.Width;
+            button1.Location = new Point(x, y);
+
+            if (checkBox.Visible)
+                checkBox.Location = new Point(FormXMargin, checkBox.Location.Y);
+        }
+
+        #endregion
+
+        #region Icon Pain
+
+        /// <summary>
+        /// Paint the System Icon in the top left corner.
+        /// </summary>
+        /// <param name="e">The paint event arguments</param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (_systemIcon != null)
+            {
+                var graphics = e.Graphics;
+                graphics.DrawIconUnstretched(_systemIcon,
+                    new Rectangle(FormXMargin, FormYMargin, _systemIcon.Width, _systemIcon.Height));
+            }
+
+            base.OnPaint(e);
+        }
+
+        #endregion
+
+        #region Result API
+
+        /// <summary>
+        /// If visible checkbox was checked.
+        /// </summary>
+        public bool CheckboxChecked
+        {
+            get { return checkBox.Checked; }
+        }
+
+        /// <summary>
+        /// Gets the button that was pressed.
+        /// </summary>
+        public MessageBoxResult MessageBoxResult { get; private set; }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (sender == button1)
+            {
+                if (button1.DialogResult != DialogResult.None)
+                    MessageBoxResult = (MessageBoxResult) button1.DialogResult;
+                else
+                    MessageBoxResult = MessageBoxResult.Button1;
+            }
+            else if (sender == button2)
+            {
+                if (button2.DialogResult != DialogResult.None)
+                    MessageBoxResult = (MessageBoxResult) button2.DialogResult;
+                else
+                    MessageBoxResult = MessageBoxResult.Button2;
+            }
+            else if (sender == button3)
+            {
+                if (button3.DialogResult != DialogResult.None)
+                    MessageBoxResult = (MessageBoxResult) button3.DialogResult;
+                else
+                    MessageBoxResult = MessageBoxResult.Button3;
+            }
+
+            Close();
+        }
+
+        #endregion
+    }
+
+    public enum MessageBoxResult
+    {
+        None = DialogResult.None,
+        OK = DialogResult.OK,
+        Cancel = DialogResult.Cancel,
+        Abort = DialogResult.Abort,
+        Retry = DialogResult.Retry,
+        Ignore = DialogResult.Ignore,
+        Yes = DialogResult.Yes,
+        No = DialogResult.No,
+        Button1,
+        Button2,
+        Button3
+    }
+}
---
+++ trunk/Solutions/CslaGenFork/Controls/MessageBoxEx.designer.cs
@@ -0,0 +1,135 @@
+﻿namespace CslaGenerator.Controls
+{
+    partial class MessageBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MessageBox));
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageLabel.Location = new System.Drawing.Point(58, 10);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(73, 16);
+            this.messageLabel.TabIndex = 19;
+            this.messageLabel.Text = "[Message]";
+            // 
+            // checkBox
+            // 
+            this.checkBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBox.AutoSize = true;
+            this.checkBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox.Location = new System.Drawing.Point(12, 114);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(152, 20);
+            this.checkBox.TabIndex = 22;
+            this.checkBox.Text = "Don\'t show this again";
+            this.checkBox.UseVisualStyleBackColor = true;
+            this.checkBox.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSize = true;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(236, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(317, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(398, 114);
+            this.button3.Name = "btn3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // messageBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(485, 149);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.messageLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MessageBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "[Title]";
+            this.Load += new System.EventHandler(this.DialogBox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
