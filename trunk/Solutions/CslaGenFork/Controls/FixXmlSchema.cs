e9a2422c7fd94dcd715a6cb67e72616ed632a73e
tiago_cp <SND\tiago_cp>
2016-06-19 23:45:56 +0000

    Rename, fix and improve version handler. Add form to select replacement for discontinued options. Fix the version upgrade algorithm. Work Item #1970

---
+++ trunk/Solutions/CslaGenFork/Controls/FixXmlSchema.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace CslaGenerator.Controls
+{
+    public partial class FixXmlSchema : Form
+    {
+        #region Fields and Properties
+
+        public string FilePath { get; set; }
+        public int OfendingLine { get; set; }
+        public int OfendingPosition { get; set; }
+        public string OfendedType { get; set; }
+        public string OriginalItem { get; set; }
+        public string ReplacementItem { get; private set; }
+
+        #endregion
+
+        public FixXmlSchema()
+        {
+            InitializeComponent();
+        }
+
+        private void FixXmlSchema_Load(object sender, EventArgs e)
+        {
+            ProcessException();
+            var executingAssembly = Assembly.GetExecutingAssembly();
+
+            foreach (Type type in executingAssembly.GetTypes())
+            {
+                if (type.IsEnum && type.Name == OfendedType)
+                {
+                    foreach (var name in Enum.GetNames(type))
+                    {
+                        listBox1.Items.Add(name);
+                    }
+                    break;
+                }
+            }
+        }
+
+        private void fixButton_Click(object sender, EventArgs e)
+        {
+            ReplacementItem = listBox1.SelectedItem as string;
+            if (!string.IsNullOrEmpty(ReplacementItem))
+                Close();
+            else
+                MessageBox.Show(@"You must select one item in the list.",
+                    @"Select replacement",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+        }
+
+        private void ProcessException()
+        {
+            label1.Text = @"Processing file:" + Environment.NewLine + FilePath;
+
+            label2.Text = string.Format("There is an error in XML document: Line {0}, Position {1}.",
+                OfendingLine,
+                OfendingPosition);
+
+            label3.Text = string.Format("Invalid value for enum '{0}'.", OfendedType);
+
+            label5.Text = string.Format(@"Choose a replacement for '{0}'", OriginalItem);
+        }
+    }
+}
