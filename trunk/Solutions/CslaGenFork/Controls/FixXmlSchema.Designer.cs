8f2768cce0352fc22772f40c740568a29a5fe8bb
Tiago Freitas Leal <tfreitasleal@gmail.com>
2016-06-19 23:45:56 +0000

    Rename, fix and improve version handler. Add form to select replacement for discontinued options. Fix the version upgrade algorithm. Work Item #1970

---
+++ trunk/Solutions/CslaGenFork/Controls/FixXmlSchema.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace CslaGenerator.Controls
+{
+    partial class FixXmlSchema
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.fixButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Full project file path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(268, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "There is an error in XML document: Line llll, Position pp.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(211, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Invalid value for enum \'OrdinaryEnumType\'.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(174, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "CslaGenFork can fix the file for you!";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(193, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Choose a replacement for \'Wrongvalue\'";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 156);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(440, 264);
+            this.listBox1.TabIndex = 4;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(15, 451);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // fixButton
+            // 
+            this.fixButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.fixButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.fixButton.Location = new System.Drawing.Point(380, 451);
+            this.fixButton.Name = "fixButton";
+            this.fixButton.Size = new System.Drawing.Size(75, 23);
+            this.fixButton.TabIndex = 0;
+            this.fixButton.Text = "Fix";
+            this.fixButton.UseVisualStyleBackColor = true;
+            this.fixButton.Click += new System.EventHandler(this.fixButton_Click);
+            // 
+            // FixXmlSchema
+            // 
+            this.AcceptButton = this.fixButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(467, 486);
+            this.Controls.Add(this.fixButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FixXmlSchema";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Loading File Error";
+            this.Load += new System.EventHandler(this.FixXmlSchema_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button fixButton;
+    }
+}
---
+++ trunk/Solutions/CslaGenFork/VersionHelper.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CslaGenerator
+{
+    internal static class VersionHelper
+    {
+        internal const string CurrentFileVersion = "4.0.4";
+        private static string[] FileLines { get; set; }
+        private static string _fileVersionFound;
+
+        public static void WriteAllLinesBetter(string path, params string[] lines)
+        {
+            if (path == null)
+                throw new ArgumentNullException("path");
+            if (lines == null)
+                throw new ArgumentNullException("lines");
+
+            using (var stream = File.OpenWrite(path))
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    if (lines.Length > 0)
+                    {
+                        for (var i = 0; i < lines.Length - 1; i++)
+                        {
+                            writer.WriteLine(lines[i]);
+                        }
+                        writer.Write(lines[lines.Length - 1]);
+                    }
+                }
+            }
+        }
+
+        internal static void SolveVersionNumberIssues(string filePath, Exception previousException)
+        {
+            FileLines = File.ReadAllLines(filePath);
+            var fileVersion = ExtractFileVersion();
+
+            if (fileVersion == CurrentFileVersion)
+                throw previousException;
+
+            var numericFileVersion = GetNumericFileVersion(fileVersion);
+            HandleFileVersion(numericFileVersion);
+            WriteAllLinesBetter(filePath, FileLines);
+        }
+
+        internal static string ExtractFileVersion()
+        {
+            var fileVersion = String.Empty;
+            foreach (var line in FileLines)
+            {
+                var lineStart = line.IndexOf(@"<FileVersion>", StringComparison.InvariantCulture);
+                if (lineStart != -1)
+                {
+                    lineStart += 13;
+                    var length = line.IndexOf(@"</FileVersion>", StringComparison.InvariantCulture) - lineStart;
+                    fileVersion = line.Substring(lineStart, length);
+                    break;
+                }
+            }
+
+            _fileVersionFound = fileVersion;
+            return fileVersion;
+        }
+
+        internal static int GetNumericFileVersion(string fileVersion)
+        {
+            var noDotsFileVersion = fileVersion.Replace(".", String.Empty);
+            int convertedFileVersion;
+
+            try
+            {
+                convertedFileVersion = Convert.ToInt32(noDotsFileVersion);
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+
+            return convertedFileVersion;
+        }
+
+        internal static void HandleFileVersion(int numericFileVersion)
+        {
+            if (numericFileVersion == 0)
+                InsertFileVersionZero();
+
+            if (numericFileVersion < 403)
+            {
+                ConvertPre403();
+                Convert403To404();
+            }
+
+            if (numericFileVersion < 404)
+                Convert403To404();
+        }
+
+        private static void InsertFileVersionZero()
+        {
+            var newFileVersion = "0.0.0";
+            var replaceString = @"<FileVersion>" + newFileVersion + "</FileVersion>";
+
+            var fileVersionPosition = FileLines.Length - 3;
+            if (FileLines[fileVersionPosition].Contains("<FileVersion />"))
+            {
+                FileLines[fileVersionPosition] = FileLines[fileVersionPosition].Replace(@"<FileVersion />",
+                    replaceString);
+            }
+            else
+            {
+                var fileLinesList = FileLines.ToList();
+                fileLinesList.Add(String.Empty);
+                FileLines = fileLinesList.ToArray();
+                fileVersionPosition = FileLines.Length - 3; // recalculate on the new array
+                FileLines[fileVersionPosition + 2] = FileLines[fileVersionPosition + 1];
+                FileLines[fileVersionPosition + 1] = FileLines[fileVersionPosition];
+                FileLines[fileVersionPosition] = "  " + replaceString;
+            }
+
+            _fileVersionFound = newFileVersion;
+        }
+
+        private static void ConvertPre403()
+        {
+            var newFileVersion = "4.0.3";
+            for (var index = 0; index < FileLines.Length; index++)
+            {
+                FileLines[index] = FileLines[index].Replace(@"<RelationType>MultipleToMultiple</RelationType>",
+                    @"<RelationType>ManyToMany</RelationType>");
+                FileLines[index] = FileLines[index].Replace(@"<RelationType>OneToMultiple</RelationType>",
+                    @"<RelationType>OneToMany</RelationType>");
+                FileLines[index] = FileLines[index].Replace(@"<FileVersion>" + _fileVersionFound + "</FileVersion>",
+                    @"<FileVersion>" + newFileVersion + "</FileVersion>");
+            }
+
+            _fileVersionFound = newFileVersion;
+        }
+
+        private static void Convert403To404()
+        {
+            var newFileVersion = "4.0.4";
+            for (var index = 0; index < FileLines.Length; index++)
+            {
+                FileLines[index] = FileLines[index].Replace(@"<Implements />", @"<Interfaces />");
+                FileLines[index] = FileLines[index].Replace(@"<Implements>", @"<Interfaces>");
+                FileLines[index] = FileLines[index].Replace(@"</Implements>", @"</Interfaces>");
+                /*FileLines[index] = FileLines[index].Replace(@"<TargetFramework>CSLA10</TargetFramework>",
+                    @"<TargetFramework>CSLA40</TargetFramework>");
+                FileLines[index] = FileLines[index].Replace(@"<TargetFramework>CSLA20</TargetFramework>",
+                    @"<TargetFramework>CSLA40</TargetFramework>");
+                FileLines[index] = FileLines[index].Replace(@"<TargetFramework>CSLA35</TargetFramework>",
+                    @"<TargetFramework>CSLA40</TargetFramework>");*/
+                FileLines[index] = FileLines[index].Replace(@"<FileVersion>" + _fileVersionFound + "</FileVersion>",
+                    @"<FileVersion>" + newFileVersion + "</FileVersion>");
+            }
+
+            _fileVersionFound = newFileVersion;
+        }
+
+        internal static int ReplaceEnum(string filePath, string originalItem, string replacementItem)
+        {
+            var counter = 0;
+
+            FileLines = File.ReadAllLines(filePath);
+
+            var search = string.Format(">{0}</", originalItem);
+            var replace = string.Format(">{0}</", replacementItem);
+
+            for (var index = 0; index < FileLines.Length; index++)
+            {
+                if (FileLines[index].Contains(search))
+                {
+                    FileLines[index] = FileLines[index].Replace(search, replace);
+                    counter++;
+                }
+            }
+
+            WriteAllLinesBetter(filePath, FileLines);
+
+            return counter;
+        }
+    }
+}
