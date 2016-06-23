cbb92a742a90a080fcb5464124feddbc6c9ca1f0
Tiago Freitas Leal <tfreitasleal@gmail.com>
2016-06-23 13:41:50 +0000

    Improve Enum display. Work Item #1985

--- trunk/Solutions/CslaGenFork/Metadata/PersistenceType.cs
+++ trunk/Solutions/CslaGenFork/Metadata/PersistenceType.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel;
+
 namespace CslaGenerator.Metadata
 {
     public enum PersistenceType
     {
+        [Description("SQL Connection Manager")]
         SqlConnectionManager = 1,
+        [Description("SQL Connection Unshared")]
         SqlConnectionUnshared = 2,
         LinqContext = 3,
         EFContext = 4
