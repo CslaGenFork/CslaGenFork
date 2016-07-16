9b13193d3bbad00efad8f17f8e1b8c71c34f9b6f
Tiago Freitas Leal <tfreitasleal@gmail.com>
2016-07-16 18:49:15 +0000

    Property default value: ignore $ property reference when not using DataPortal_Create. Work Item #2004

--- trunk/Solutions/CslaGenFork/CodeGen/CslaTemplateHelperCS.cs
+++ trunk/Solutions/CslaGenFork/CodeGen/CslaTemplateHelperCS.cs
@@ -2574,7 +2574,12 @@ namespace CslaGenerator.CodeGen
                 return string.Empty;
 
             if (prop.DefaultValue != string.Empty)
+            {
+                if (prop.DefaultValue.IndexOf("$", StringComparison.InvariantCulture) == 0)
+                    return string.Empty;
+
                 return ", " + prop.DefaultValue;
+            }
 
             if (!prop.Nullable)
                 return string.Empty;
