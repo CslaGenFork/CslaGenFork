1d04ebcf754fc8516fa3eab631447fdf168a6f00
Tiago Freitas Leal <tfreitasleal@gmail.com>
2017-01-22 10:33:01 +0000

    Improve code generation coverage tests. Work Item #2025

---
+++ trunk/CoverageTest/UsingClass/MyBusinessBaseT.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Csla;
+
+namespace UsingClass
+{
+    [Serializable]
+    public class MyBusinessBase<T> : BusinessBase<T>
+        where T : MyBusinessBase<T>
+    {
+    }
+}
