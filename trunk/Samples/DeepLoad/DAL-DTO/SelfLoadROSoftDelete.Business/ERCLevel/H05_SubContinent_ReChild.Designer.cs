1996da04e60a44720f9861b34d0510bb4b9d2bac
Tiago Freitas Leal <tfreitasleal@gmail.com>
2017-01-31 00:15:07 +0000

    DeepLoad sample: updated code generation. Work Item #1571

--- trunk/Samples/DeepLoad/DAL-DTO/SelfLoadROSoftDelete.Business/ERCLevel/H05_SubContinent_ReChild.Designer.cs
+++ trunk/Samples/DeepLoad/DAL-DTO/SelfLoadROSoftDelete.Business/ERCLevel/H05_SubContinent_ReChild.Designer.cs
@@ -54,9 +54,10 @@ namespace SelfLoadROSoftDelete.Business.ERCLevel
         /// Initializes a new instance of the <see cref="H05_SubContinent_ReChild"/> class.
         /// </summary>
         /// <remarks> Do not use to create a Csla object. Use factory methods instead.</remarks>
-        private H05_SubContinent_ReChild()
+        [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+        public H05_SubContinent_ReChild()
         {
-            // Prevent direct creation
+            // Use factory methods and do not use direct creation.
         }
 
         #endregion
@@ -96,7 +97,7 @@ namespace SelfLoadROSoftDelete.Business.ERCLevel
 
         #endregion
 
-        #region Pseudo Events
+        #region DataPortal Hooks
 
         /// <summary>
         /// Occurs after setting query parameters and before the fetch operation.
--- trunk/Samples/DeepLoad/DAL-DTO/SelfLoadROSoftDelete.Business/ERLevel/G03_Continent_ReChild.Designer.cs
+++ trunk/Samples/DeepLoad/DAL-DTO/SelfLoadROSoftDelete.Business/ERLevel/G03_Continent_ReChild.Designer.cs
@@ -54,9 +54,10 @@ namespace SelfLoadROSoftDelete.Business.ERLevel
         /// Initializes a new instance of the <see cref="G03_Continent_ReChild"/> class.
         /// </summary>
         /// <remarks> Do not use to create a Csla object. Use factory methods instead.</remarks>
-        private G03_Continent_ReChild()
+        [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+        public G03_Continent_ReChild()
         {
-            // Prevent direct creation
+            // Use factory methods and do not use direct creation.
         }
 
         #endregion
@@ -96,7 +97,7 @@ namespace SelfLoadROSoftDelete.Business.ERLevel
 
         #endregion
 
-        #region Pseudo Events
+        #region DataPortal Hooks
 
         /// <summary>
         /// Occurs after setting query parameters and before the fetch operation.
