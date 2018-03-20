f0337e47d7d48f6ce3c9afedb79e0ab83803673c
tfreitasleal <tfreitasleal@gmail.com>
2018-03-20 19:20:32 +0000

    Update DockPanelSuite to 3.0.4

--- trunk/Solutions/CslaGenFork/MainForm.designer.cs
+++ trunk/Solutions/CslaGenFork/MainForm.designer.cs
@@ -198,7 +198,6 @@ namespace CslaGenerator
             dockPaneStripToolWindowGradient1.InactiveTabGradient = tabGradient7;
             dockPaneStripSkin1.ToolWindowGradient = dockPaneStripToolWindowGradient1;
             dockPanelSkin1.DockPaneStripSkin = dockPaneStripSkin1;
-            this.dockPanel.Skin = dockPanelSkin1;
             this.dockPanel.TabIndex = 2;
             // 
             // menuStrip
