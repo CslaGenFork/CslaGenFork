b415e181dae4346fd8d5fce8293616cb5c1f3efe
tfreitasleal <tfreitasleal@gmail.com>
2018-03-22 18:38:40 +0000

    Fix generic type definition.

--- trunk/CoverageTest/UsingLibrary/MyDynamicBindingListBaseT.cs
+++ trunk/CoverageTest/UsingLibrary/MyDynamicBindingListBaseT.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using Csla;
 using Csla.Core;
+using Csla.Serialization.Mobile;
 
 namespace UsingLibrary
 {
     [Serializable]
     public class MyDynamicBindingListBase<T> : DynamicBindingListBase<T>
-        where T : IEditableBusinessObject, IUndoableObject, ISavable, IBusinessObject
+        where T : IEditableBusinessObject, IUndoableObject, ISavable, IMobileObject, IBusinessObject
     {
     }
 }
