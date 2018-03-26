889ced594e05661c8a479fd28a218f1fe3e0ea73
tfreitasleal <tfreitasleal@gmail.com>
2018-03-26 17:06:32 +0100

    Add Coverage Tests Support for Improve criteria classes inheriting from BusinessBase (#58)

--- trunk/CoverageTest/InterwayDocs/DAL-DTO/Codisa.InterwayDocs.DataAccess/DalFactoryInterwayDocs.cs
+++ trunk/CoverageTest/InterwayDocs/DAL-DTO/Codisa.InterwayDocs.DataAccess/DalFactoryInterwayDocs.cs
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------
-// <copyright file="DalFactory.cs" company="Marimer LLC">
+// <copyright file="DalFactoryInterwayDocs.cs" company="Marimer LLC">
 //     Copyright (c) Marimer LLC. All rights reserved.
 //     Website: http://www.lhotka.net/cslanet/
 // </copyright>
@@ -11,35 +11,35 @@ using System.Configuration;
 namespace Codisa.InterwayDocs.DataAccess
 {
     /// <summary>
-    /// Creates a  DAL manager provider.
+    /// Creates a InterwayDocs DAL manager provider.
     /// </summary>
     /// <remarks>
     /// To use the generated DAL:<br/>
     /// 1) name this assembly Codisa.InterwayDocs.DataAccess<br/>
     /// 2) add the following line to the <strong>appSettings</strong>
     /// section of the application .config file: <br/>
-    /// &lt;add key=".DalManagerType" value="Codisa.InterwayDocs.DataAccess.Sql.DalManager, Codisa.InterwayDocs.DataAccess.Sql" /&gt;
+    /// &lt;add key="InterwayDocs.DalManagerType" value="Codisa.InterwayDocs.DataAccess.Sql.DalManagerInterwayDocs, Codisa.InterwayDocs.DataAccess.Sql" /&gt;
     /// </remarks>
-    public static class DalFactory
+    public static class DalFactoryInterwayDocs
     {
         private static Type _dalType;
 
-        /// <summary>Gets the  DAL manager type that must be set
+        /// <summary>Gets the InterwayDocs DAL manager type that must be set
         /// in the <strong>appSettings</strong> section of the application .config file.</summary>
-        /// <returns>A new <see cref="IDalManager"/> instance</returns>
-        public static IDalManager GetManager()
+        /// <returns>A new <see cref="IDalManagerInterwayDocs"/> instance</returns>
+        public static IDalManagerInterwayDocs GetManager()
         {
             if (_dalType == null)
             {
-                var dalTypeName = ConfigurationManager.AppSettings[".DalManagerType"];
+                var dalTypeName = ConfigurationManager.AppSettings["InterwayDocs.DalManagerType"];
                 if (!string.IsNullOrEmpty(dalTypeName))
                     _dalType = Type.GetType(dalTypeName);
                 else
-                    throw new NullReferenceException(".DalManagerType");
+                    throw new NullReferenceException("InterwayDocs.DalManagerType");
                 if (_dalType == null)
                     throw new ArgumentException(string.Format("Type {0} could not be found", dalTypeName));
             }
-            return (IDalManager) Activator.CreateInstance(_dalType);
+            return (IDalManagerInterwayDocs) Activator.CreateInstance(_dalType);
         }
     }
 }
