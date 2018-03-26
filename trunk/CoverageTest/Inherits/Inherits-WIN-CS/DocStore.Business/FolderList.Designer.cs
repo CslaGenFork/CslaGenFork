82e21cfc814943522d08eb43a32bf0ffe8da482a
tfreitasleal <tfreitasleal@gmail.com>
2018-03-26 21:47:10 +0100

    Re-generated

--- trunk/CoverageTest/Inherits/Inherits-WIN-CS/DocStore.Business/FolderList.Designer.cs
+++ trunk/CoverageTest/Inherits/Inherits-WIN-CS/DocStore.Business/FolderList.Designer.cs
@@ -16,7 +16,7 @@ namespace DocStore.Business
 
     /// <summary>
     /// Collection of folder's basic information (read only list).<br/>
-    /// This is a generated base class of <see cref="FolderList"/> business object.
+    /// This is a generated <see cref="FolderList"/> business object.
     /// This class is a root collection.
     /// </summary>
     /// <remarks>
